ff2efcb0972c7e93440467964cc470a1cf5cf349
Talk Is Cheap <779222056@qq.com>
2024-03-16 15:21:27 +0800

    Update get_dataset_list.py

--- get_dataset_list.py
+++ get_dataset_list.py
@@ -12,8 +12,8 @@ for tmp in ["test", "train"]:
         input_list.append(one.strip().split(":")[1])
 
     seed_count = 50
-    for the_seed in range(seed_count):
-        f_rand = open("data/explore_data/" + tmp + str(the_seed) + ".txt", encoding="utf-8", mode="w")
+    for index in range(seed_count):
+        f_rand = open("data/explore_data/" + tmp + str(index) + ".txt", encoding="utf-8", mode="w")
 
         for i in range(len(predict_labels)):
             if predict_labels != origin_labels[i]:
